715036552bfb4ea86eba6a0dad09230a7c5cac9e
ziad <helmyzez@gmail.com>
2025-11-15 19:54:59 +0300

    Fix Vercel deployment: await bootstrap and update connection

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import bootstrap from "./src/bootstrap.js";
+
 const app = express();
 
-bootstrap(app, express);
+// Initialize app
+await bootstrap(app, express);
 
 export default app;
